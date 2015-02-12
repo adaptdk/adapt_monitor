76164c56987351336c1d80579e704a0d9626d9aa
jesperjb <jesper@adapt.dk>
2015-02-12 13:29:30 +0100

    TOOLS-134 Updated styling.

--- tpl/adapt-monitor-info.tpl.php
+++ tpl/adapt-monitor-info.tpl.php
@@ -1,6 +1,6 @@
 <div class="adapt-monitor-info">
   <div class="title"><?php print $title; ?></div>
-  <div class="status"><?php print $status; ?></div>
+  <div class="status"><span><?php print $status; ?></span></div>
   <div class="build"><?php print $build; ?></div>
   <div class="version"><?php print $version; ?></div>
 </div>
