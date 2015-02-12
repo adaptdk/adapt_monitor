4616ed3e1bfdf0da01783351949096c268b0bf93
jesperjb <jesper@adapt.dk>
2015-02-12 13:11:44 +0100

    TOOLS-134 Initial commit.

---
+++ tpl/adapt-monitor-info.tpl.php
@@ -0,0 +1,6 @@
+<div class="adapt-monitor-info">
+  <div class="title"><?php print $title; ?></div>
+  <div class="status"><?php print $status; ?></div>
+  <div class="build"><?php print $build; ?></div>
+  <div class="version"><?php print $version; ?></div>
+</div>
---
+++ tpl/adapt-monitor-item.tpl.php
@@ -0,0 +1,4 @@
+<div class="adapt-monitor-item">
+  <div class="name"><?php print $name; ?></div>
+  <div class="version"><?php print $version; ?></div>
+</div>
